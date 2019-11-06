e85673bbc3f5237dc68425312f3173d9901fe002
Massimo2511 <maxym.panichek@gmail.com>
2019-11-06 21:01:38 +0200

    First task

--- src/com/company/FirstTasks/Main.java
+++ src/com/company/FirstTasks/Main.java
@@ -1,10 +1,29 @@
 package com.company.FirstTasks;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+       /*Task 1
+        1 . Написать метод который принимает в себя текст сезон (“summer”, “winter”...) - переменную типа String,
+         и если сезон - winter выводит в консоль “It is cold”.*/
 
-        System.out.println("This is new for me");
+
+        System.out.println("Task 1 'SEASON'");
+        System.out.println("Enter the name of the season that start from capital letter");
+
+
+        Scanner keyboard = new Scanner(System.in);
+        String width = keyboard.next();
+
+        System.out.println("You have entered season: ");
+        System.out.println(width);
+
+        Counter ss = new Counter();
+        ss.season(width);
+
+
 	// write your code here
     }
 }

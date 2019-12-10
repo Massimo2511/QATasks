6d2721f81845c2603fb0e23347a78865c2b7c9ee
Massimo2511 <maxym.panichek@gmail.com>
2019-12-10 15:31:14 +0200

    update2

--- src/com/company/RoundFourTasks/Manager.java
+++ src/com/company/RoundFourTasks/Manager.java
@@ -1,22 +1,9 @@
 package com.company.RoundFourTasks;
 
-public class Manager extends FactoryProduct {
-
-    // Переоприділив конструктор додавши "int humanHours"
-    public Manager(String materialType1, int materialQuantity1, String materialType2, int materialQuantity2, int humanHours) {
-        super(materialType1, materialQuantity1, materialType2, materialQuantity2);
-    }
-
-    public Manager(int manHours, int workersQuantity) {
-        super(manHours, workersQuantity);
-    }
-
-    public Manager(double weight, double length, double width, double height) {
-        super(weight, length, width, height);
-    }
+public class Manager  {
 
     public static void main(String[] args) {
-       System.out.println();
+
 
        Chairs.result();
        Wodrobes.result();
